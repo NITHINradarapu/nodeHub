1f0081bce0e1f7849ae220e8cfefabf8e5203597
NITHINradarapu <radarapunithin05@gmail.com>
2026-03-26 22:02:44 +0530

    Implemented addRepo

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -2,12 +2,16 @@ const fs = require('fs').promises;
 const path = require('path');
 
 
-async function addRepo(){
+async function addRepo(filePath){
     const addPath = path.resolve(process.cwd(), ".apnaGit");
-    const staging = path.join(addPath, "staging");
+    const stagingPath = path.join(addPath, "staging");
 
     try{
-        
+        await fs.mkdir(stagingPath, {recursive : true});
+        const fileName = path.basename(filePath);
+        const destPath = path.join(stagingPath, fileName);
+        await fs.copyFile(filePath, destPath);
+        console.log(`File ${fileName} added to staging area`);
     }catch(error){
         console.error("Error adding file: ", error);
     }
